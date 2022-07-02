646509b09b1ce55439a86b6cc6b63393eda175d7
joostone-ahn <98713651+joostone-ahn@users.noreply.github.com>
2022-07-03 02:54:51 +0900

    v1.2 2022-07-03

---
+++ Proactive.py
@@ -0,0 +1,36 @@
+# ETSI TS 102.223
+
+Proactive_type = dict()
+
+Proactive_type['01'] = 'REFRESH'
+Proactive_type['03'] = 'POLL INTERVAL'
+Proactive_type['05'] = 'SET UP EVENT LIST'
+Proactive_type['25'] = 'SET UP MENU'
+Proactive_type['40'] = 'OPEN CHANNEL'
+Proactive_type['41'] = 'CLOSE CHANNEL'
+Proactive_type['42'] = 'RECEIVE DATA'
+Proactive_type['43'] = 'SEND DATA'
+
+REFRESH_type = dict()
+# REFRESH_type['00'] = 'NAA Initialization and Full File Change Notification'
+# REFRESH_type['01'] = 'File Change Notification'
+# REFRESH_type['02'] = 'NAA Initialization and File Change Notification'
+# REFRESH_type['03'] = 'NAA Initialization'
+REFRESH_type['04'] = 'UICC Reset'
+# REFRESH_type['05'] = 'NAA Application Reset, only applicable for a 3G platform'
+# REFRESH_type['06'] = 'NAA Session Reset, only applicable for a 3G platform'
+# REFRESH_type['07'] = 'Steering of Roaming'
+# REFRESH_type['09'] = 'eUICC Profile State Change'
+# REFRESH_type['0A'] = 'Application Update'
+
+
+
+
+
+
+
+
+
+
+
+
